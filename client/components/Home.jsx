13452238fcfe4d611d5174fb808cfc0dd41c2e18
Nat Sirisamphan <nat_sirisamphan@hotmail.com>
2018-09-13 16:22:27 +1200

    update

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,12 +3,10 @@ import React from 'react'
 function Home () {
     return (
         <div>          
-            <h1 class="top-left">Noon & Craig's Wedding </h1>  
-            <h1 class="bottom-left">Marriage</h1>
-            
+            <h1 class="centered">Noon & Craig's Wedding</h1>  
+            <h1 class="centered2">Marriage</h1>  
+                
         </div>
-        
-       
      )
 }
 export default Home
