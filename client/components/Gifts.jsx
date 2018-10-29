31c44cad738ce2ee614b31496aaf8e26e8d95544
Nat Sirisamphan <nat_sirisamphan@hotmail.com>
2018-10-29 21:15:37 +1300

    fixing information page

--- client/components/Gifts.jsx
+++ client/components/Gifts.jsx
@@ -4,6 +4,7 @@ function Gifts () {
     return (
         <div>          
             <h1 className="top-left">Gifts page</h1>  
+            <p className="test">Hellooo for gifts please.... etc etc etcetc etc etc etc etc etc etc etc etc etc etc etc etc etc etc etc etc etc  </p>
         </div>
      )
 }
--- client/components/Thanks.jsx
+++ client/components/Thanks.jsx
@@ -6,10 +6,13 @@ import Home from './Home'
 function Thanks () {
     
     return (
-        <div>          
-            <h1>Thank you for RSVP'ING</h1>  
-            <h1 class="centered3">We will keep you updated via e-mail when new information does come up.</h1>  
+        <div>           
+            <h1 class="centered3">Thank you for RSVP'ING. Please find more information on the information page.</h1>  
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <a className="nav-item nav-link" href='#' onClick={()=>{console.log('clicliclci'); return(Router.refresh()) }}>HOME</a>
+            <a className="nav-item nav-link" href="#Information">Information</a>
+            <a className="nav-item nav-link" href="#Gifts">Gifts</a>
+        </div>
             
         </div>
      )
