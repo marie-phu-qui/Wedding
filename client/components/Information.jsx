a49d62a1bc217e023b70f71a2def2fb31c03f600
Nat Sirisamphan <nat_sirisamphan@hotmail.com>
2018-11-06 21:47:20 +1300

    css for information page and data

--- client/components/Information.jsx
+++ client/components/Information.jsx
@@ -8,10 +8,14 @@ function Information () {
             <p className="info2">and</p> 
             <p className="info3">Craig Sefton</p> 
             <p className="info4">request the pleasure of your company to celebrate their wedding</p> 
-
-            <p></p>
-
-
+            <p className="firstline"><hr/></p>
+            <p className="info5">Saturday 27 April 2019 at 2:00pm</p>
+            <p className="secondline"><hr/></p>
+            <p className="info6">Te Kairanga Winery,</p>
+            <p className="info7">89 Martins Road,</p>
+            <p className="info8">Martinborough, New Zealand</p>
+            <p className="secondline"><hr/></p>
+            <p className="info9">Dinner and dancing to follow.</p>
         </div>
      )
 }
--- client/components/Thanks.jsx
+++ client/components/Thanks.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import {HashRouter as Router, Route, Link, Redirect} from 'react-router-dom'
 
 import Home from './Home'
+import Information from './Information'
+import Rsvp from './Rsvp'
+
+
+
 
 function Thanks () {
     
@@ -9,6 +14,8 @@ function Thanks () {
         <div>           
             <h1 class="centered3">Thank you for your RSVP.  Check your inbox for a confirmation email.  Please check our information page as we will update it with more details as they become available.</h1> 
             <a className="nav-item nav-link" href='#' onClick={()=>{return(Router.refresh())}}>Home</a>
+            <a className="nav-item nav-link" href='#information' onClick={()=>{return(Router.refresh())}}>Information</a>
+            
         </div>
      )
 }
